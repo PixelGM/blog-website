149b60318cc2424c1eef5864ea93b5460d4feec2
ピクセルGM <amosprayogo@gmail.com>
2024-02-15 00:12:16 -0800

    Implemented Gap SCSS

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -7,7 +7,7 @@ export default createBoard({
     environmentProps: {
         canvasWidth: 2090,
         canvasHeight: 1175,
-        windowWidth: 996,
+        windowWidth: 1920,
         windowHeight: 1080,
     },
     isSnippet: false,
